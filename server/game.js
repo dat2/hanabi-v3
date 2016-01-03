372c51b0c3b41a435b20b5c1b2844e2788739ced
Nicholas Dujay <ndujay05@ca.ibm.com>
2016-01-02 20:31:27 -0500

    nearly 100% coverage

--- server/game.js
+++ server/game.js
@@ -15,7 +15,13 @@ const jsAtom = require('js-atom');
 /**
  * There are 3 1s, 2 2s, 2 3s, 2 4s, 1 5.
  */
-const DEFAULT_WEIGHTS = [ [1, 1], [2, 1], [3, 1], [4, 1], [5, 1] ];
+const DEFAULT_WEIGHTS = {
+  1: 3,
+  2: 2,
+  3: 2,
+  4: 2,
+  5: 1
+};
 /**
  * The default colours are red, blue, yellow, white and green
  * @type {Array}
@@ -34,7 +40,7 @@ function generateDeck(seed) {
   const colours = DEFAULT_COLOURS;
   // generate a list of the numbers, weighted
   // [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
-  const nums = R.flatten(R.map(w => R.repeat(w[0], w[1]), weights));
+  const nums = R.flatten(R.map(w => R.repeat(parseInt(w[0]), w[1]), R.toPairs(weights)));
   // xprod will do a permutation of all combinations of numbers and colours
   // zipObj: [['number', 'colour'], [1, 'red']] => { number: 1, colour: 'red' }
   const deck = R.map(R.zipObj(['number', 'colour']), R.xprod(nums, colours));
@@ -52,13 +58,12 @@ function makeGameState(seed) {
   return {
     // players
     players: {},
-    currentlyPlaying: 0,
+    currentPlayer: '',
 
     gameOver: false,
     // resources
     lives: 3,
     info: 8,
-    infoMode: 'neither',
 
     // cards
     deck: generateDeck(seed),
@@ -73,9 +78,22 @@ const removeLife = update('lives', R.compose(R.max(0), R.dec));
 const addInfo = update('info', R.compose(R.min(8), R.inc));
 const removeInfo = update('info', R.dec);
 
-const setInfoModeToColour = R.assoc('infoMode', 'colour');
-const setInfoModeToNumber = R.assoc('infoMode', 'number');
-const getInfoMode = get('infoMode');
+const endGame = R.assoc('gameOver', true);
+
+const setFirstPlayer = state => {
+  const first = R.compose(R.head, getPlayers)(state);
+  return R.assoc('currentPlayer', first)(state);
+};
+const setNextPlayer = state => {
+  const players = getPlayers(state);
+  const player = getCurrentPlayer(state);
+
+  const playerIndex = R.indexOf(player, players);
+  const nextIndex = (playerIndex + 1) % players.length;
+
+  return R.assoc('currentPlayer', players[nextIndex])(state);
+};
+const getCurrentPlayer = get('currentPlayer');
 
 // stack functions
 const getTopOfStack = colour => getPath(['stacks', colour, 0]);
@@ -102,13 +120,13 @@ const removeCardFrom = R.curry((name, i) => updatePath(['players', name, 'cards'
 const getCardAt = R.curry((name, card) => getPath(['players', name, 'cards', card]));
 const getPlayers = R.compose(Object.keys, getPath(['players']));
 
-const addKnownColour = R.curry((name, colour) =>
+const addKnownColour = R.curry((colour, name) =>
   updatePath(
     ['players', name, 'knownColours'],
     R.compose(R.uniq, R.append(colour))
   )
 );
-const addKnownNumber = R.curry((name, number) =>
+const addKnownNumber = R.curry((number, name) =>
   updatePath(
     ['players', name, 'knownNumbers'],
     R.compose(R.uniq, R.append(number))
@@ -119,81 +137,102 @@ const addKnownNumber = R.curry((name, number) =>
 const peekDeck = getPath(['deck', 0]);
 const popDeck = update('deck', R.tail);
 
-const addToDiscard = card => update('discard', R.append(card));
-const dealTo = R.curry(
-  (name, state) =>
-    R.compose(
-      addCardTo(name, peekDeck(state)),
-      popDeck
-    )(state)
-);
+const addToDiscard = card => update('discard', R.prepend(card));
+const dealTo = R.curry((name, state) =>
+  R.compose(
+    addCardTo(name, peekDeck(state)),
+    popDeck
+  )(state));
 
 // deal to each player n cards
 const dealCards = R.curry((n, state) => {
   const players = getPlayers(state);
   const ps = R.flatten(R.repeat(players, n));
-  R.forEach(name => dealTo(name, state), ps);
-  return state;
+  // convert each player's name to a function, that when applied to the state
+  // will add the card from the top of the deck to the players cards
+  // and compose each function into one large function
+  return R.apply(R.compose, R.map(name => dealTo(name), ps))(state);
+});
+
+const checkGameOver = R.curry((card, state) => {
+  // check if this is the last card in the discard
+  const discard = get('discard')(state);
+
+  const cardToString = card => `${card.number}-${card.colour}`;
+
+  const getWeight = card => DEFAULT_WEIGHTS[card.number];
+
+  const cardCounts = R.countBy(cardToString, discard);
+  const str = cardToString(card);
+  const weight = getWeight(card);
+
+  // if adding this card to the discard pile would put all of this card
+  // eg. green-4 into the discard pile, then return true
+  return (cardCounts[str] || 0) + 1 >= weight;
 });
 
 // (1) DISCARD
-const discard = R.curry((player, card, state) => {
+const discard = R.curry((card, state) => {
+  const player = getCurrentPlayer(state);
   const cardObj = getCardAt(player, card)(state);
 
+  var fn = R.identity;
+  if(checkGameOver(cardObj, state)) {
+    fn = endGame;
+  } else {
+    fn = R.compose(
+      dealTo(player),
+      addInfo
+    );
+  }
+
   return R.compose(
+    fn,
     addToDiscard(cardObj),
     removeCardFrom(player, card),
-    dealTo(player),
-    addInfo
+    setNextPlayer
   )(state);
 });
 
 // (2) PLAY
-const play = R.curry((player, card, state) => {
-  var fn = R.identity;
-  if(isValidMove(getCardAt(player, card)(state), state)) {
-    fn = R.compose(
-      putOnStack(cardObj.colour, cardObj.number),
+const play = R.curry((card, state) => {
+  const player = getCurrentPlayer(state);
+  const cardObj = getCardAt(player, card)(state);
 
-      // same for both
-      removeCardFrom(player, card),
-      dealTo(player)
-    );
+  var fn = R.identity;
+  if(isValidMove(cardObj, state)) {
+    fn = putOnStack(cardObj.colour, cardObj.number);
   } else {
     fn = R.compose(
       addToDiscard(cardObj),
-      removeLife,
-
-      // same for both
-      removeCardFrom(player, card),
-      dealTo(player)
+      removeLife
     );
   }
 
-  return fn(state);
+  return R.compose(
+    fn,
+    removeCardFrom(player, card),
+    dealTo(player),
+    setNextPlayer
+  )(state);
 });
 
 // (3) Give Info To
-const giveInfo = R.curry((player, state) => {
-  const currentMode = getInfoMode(state);
+const giveInfo = R.curry((type, player, card, state) => {
+  const cardObj = getCardAt(player, card)(state);
 
+  const fn = type === 'number' ? addKnownNumber(cardObj.number) : addKnownColour(cardObj.colour);
   return R.compose(
-
-  );
+    fn(player),
+    setNextPlayer,
+    removeInfo
+  )(state);
 });
 
 // room state functions
 function updateState(f) {
   gameState.swap(f);
 }
-
-// addPlayer('nick'));
-// addPlayer('nick2'));
-// addPlayer('nick3'));
-// addPlayer('nick4'));
-// dealCards(5);
-// play('nick4', 0);
-// printgameState();
 
 module.exports = {
   current() {
@@ -204,17 +243,24 @@ module.exports = {
   },
   start() {
     updateState(dealCards(5));
+    updateState(setFirstPlayer);
   },
   addPlayer(name) {
     updateState(addPlayer(name));
   },
+  hasPlayer(name) {
+    return getPath(['players', name], gameState.deref()) !== null;
+  },
   removePlayer(name) {
     updateState(removePlayer(name));
   },
-  play(player, card) {
-    updateState(play(player, card));
+  playCard(card) {
+    updateState(play(card));
   },
-  discard(player, card) {
-    updateState(discard(player, card));
+  discardCard(card) {
+    updateState(discard(card));
+  },
+  giveInfo(type, player, card) {
+    updateState(giveInfo(type, player, card));
   }
 };
--- server/test/gameTest.js
+++ server/test/gameTest.js
@@ -2,12 +2,14 @@ const should = require('should');
 
 const game = require('../game');
 
+// DO NOT CHANGE THIS SEED
+// tests all depend on this seed
 beforeEach(function() {
   game.reset(123123123);
 })
 
-describe('player functions', function() {
-  describe('adding players', function() {
+describe('game', function() {
+  describe('#addPlayer', function() {
     it('should have a player named `nick` when you add a player named nick', function() {
       game.addPlayer('nick');
       const state = game.current();
@@ -24,7 +26,7 @@ describe('player functions', function() {
     });
   });
 
-  describe('removing players', function() {
+  describe('#removePlayer', function() {
     it('should not have a player with the name that you just removed', function() {
       game.addPlayer('nick');
       game.removePlayer('nick');
@@ -43,11 +45,22 @@ describe('player functions', function() {
       state.players.should.not.have.property('nick');
     });
   });
+
+  describe('#hasPlayer', function() {
+    it('should return true when there is a player with that name', function() {
+      game.addPlayer('nick');
+      game.hasPlayer('nick').should.be.eql(true);
+    });
+    it('should return false when there is not a player with that name', function() {
+      game.addPlayer('nick');
+      game.hasPlayer('nick2').should.be.eql(false);
+    });
+  });
 });
 
-describe('general state', function() {
-  describe('start state', function() {
-    it('should have the correct starting state', function() {
+describe('The game state', function() {
+  describe('at the start', function() {
+    it('should have the correct misc variables', function() {
       game.addPlayer('nick');
       game.addPlayer('nick2');
       game.start();
@@ -56,12 +69,69 @@ describe('general state', function() {
       state.gameOver.should.be.eql(false);
       state.lives.should.be.eql(3);
       state.info.should.be.eql(8);
-      state.infoMode.should.be.eql('neither');
+      state.currentPlayer.should.be.eql('nick');
     });
 
-    it('should deal 5 cards to each player', function() {
+    it('should have a deck that has 1 five of each colour', function() {
+      const state = game.current();
+      state.deck.should.containEql({ number: 5, colour: 'red' });
+      state.deck.should.containEql({ number: 5, colour: 'blue' });
+      state.deck.should.containEql({ number: 5, colour: 'white' });
+      state.deck.should.containEql({ number: 5, colour: 'green' });
+      state.deck.should.containEql({ number: 5, colour: 'yellow' });
+    });
+
+    it('should have a deck that has 2 fours of each colour', function() {
+      const state = game.current();
+
+      state.deck.should.containDeep([{ number: 4, colour: 'red' },{ number: 4, colour: 'red' }]);
+      state.deck.should.containDeep([{ number: 4, colour: 'blue' },{ number: 4, colour: 'blue' }]);
+      state.deck.should.containDeep([{ number: 4, colour: 'white' },{ number: 4, colour: 'white' }]);
+      state.deck.should.containDeep([{ number: 4, colour: 'green' },{ number: 4, colour: 'green' }]);
+      state.deck.should.containDeep([{ number: 4, colour: 'yellow' },{ number: 4, colour: 'yellow' }]);
+    });
+
+    it('should have a deck that has 2 threes of each colour', function() {
+      const state = game.current();
+
+      state.deck.should.containDeep([{ number: 3, colour: 'red' },{ number: 3, colour: 'red' }]);
+      state.deck.should.containDeep([{ number: 3, colour: 'blue' },{ number: 3, colour: 'blue' }]);
+      state.deck.should.containDeep([{ number: 3, colour: 'white' },{ number: 3, colour: 'white' }]);
+      state.deck.should.containDeep([{ number: 3, colour: 'green' },{ number: 3, colour: 'green' }]);
+      state.deck.should.containDeep([{ number: 3, colour: 'yellow' },{ number: 3, colour: 'yellow' }]);
+    });
+
+    it('should have a deck that has 2 twos of each colour', function() {
+      const state = game.current();
+
+      state.deck.should.containDeep([{ number: 2, colour: 'red' },{ number: 2, colour: 'red' }]);
+      state.deck.should.containDeep([{ number: 2, colour: 'blue' },{ number: 2, colour: 'blue' }]);
+      state.deck.should.containDeep([{ number: 2, colour: 'white' },{ number: 2, colour: 'white' }]);
+      state.deck.should.containDeep([{ number: 2, colour: 'green' },{ number: 2, colour: 'green' }]);
+      state.deck.should.containDeep([{ number: 2, colour: 'yellow' },{ number: 2, colour: 'yellow' }]);
+    });
+
+    it('should have a deck that has 3 ones of each colour', function() {
+      const state = game.current();
+
+      state.deck.should.containDeep([{ number: 1, colour: 'red' },{ number: 1, colour: 'red' },{ number: 1, colour: 'red' }]);
+      state.deck.should.containDeep([{ number: 1, colour: 'blue' },{ number: 1, colour: 'blue' },{ number: 1, colour: 'blue' }]);
+      state.deck.should.containDeep([{ number: 1, colour: 'white' },{ number: 1, colour: 'white' },{ number: 1, colour: 'white' }]);
+      state.deck.should.containDeep([{ number: 1, colour: 'green' },{ number: 1, colour: 'green' },{ number: 1, colour: 'green' }]);
+      state.deck.should.containDeep([{ number: 1, colour: 'yellow' },{ number: 1, colour: 'yellow' },{ number: 1, colour: 'yellow' }]);
+    });
+
+    it('should have a deck of 50 cards', function() {
+      const state = game.current();
+      const nColours = 5;
+      const nNumbers = 3 + 2 + 2 + 2 + 1;
+      state.deck.should.have.length(nColours * nNumbers);
+    });
+
+    it('should have dealt 5 cards to each player', function() {
       game.addPlayer('nick');
       game.addPlayer('nick2');
+
       game.start();
 
       const state = game.current();
@@ -71,18 +141,253 @@ describe('general state', function() {
     });
   });
 
-  describe('playing a valid card', function() {
+  it('should update the current player correctly', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.addPlayer('nick3');
+    game.addPlayer('nick4');
+    game.start();
+
+    var state = game.current();
+    state.currentPlayer.should.be.eql('nick');
+
+    game.discardCard(0); // nick discards { number: 1, colour: 'blue' }
+    state = game.current();
+    state.currentPlayer.should.be.eql('nick2');
+
+    game.discardCard(0); // nick2 discards { number: 3, colour: 'blue' }
+    state = game.current();
+    state.currentPlayer.should.be.eql('nick3');
+
+    game.discardCard(0); // nick3 discards { number: 1, colour: 'white' }
+    state = game.current();
+    state.currentPlayer.should.be.eql('nick4');
+
+    game.discardCard(0); // nick4 discards { number: 3, colour: 'green' }
+    state = game.current();
+    state.currentPlayer.should.be.eql('nick');
+  });
+});
+
+// CHANGE THESE TESTS IF THE SEED CHANGES
+describe('playing', function() {
+  it('should deal a card back to the players hand', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.start();
+    const stateBefore = game.current();
+
+    game.playCard(0);
+    const stateAfter = game.current();
+
+    stateAfter.deck.should.have.length(stateBefore.deck.length - 1);
+    stateAfter.players.nick.cards.should.have.length(5);
+  });
+
+  it('should update the currently playing player', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.start();
+    game.playCard(0);
+    const state = game.current();
+
+    state.currentPlayer.should.be.eql('nick2');
+  });
+
+  describe('a valid card', function() {
+    // with the seed 123123123
+    // nick's first card is { number: 1, colour: 'white' }
     it('should start a stack when the stack is empty', function() {
       game.addPlayer('nick');
       game.addPlayer('nick2');
       game.start();
 
+      const before = game.current();
+      before.stacks.white.should.have.length(0);
+
+      game.playCard(0);
+      const after = game.current();
+
+      after.stacks.white.should.have.length(1);
+      after.stacks.white.should.eql([1]);
+    });
+
+    // with the seed 123123123
+    // nick's first card is { number: 1, colour: 'white' }
+    // nick2's 5th card is { number: 2, colour: 'white' }
+    it('should put on top of a stack when the stack is not empty', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+      game.playCard(0); // nick plays his first card
+      game.playCard(4); // nick2 plays his 5th card
+
       const state = game.current();
+      state.stacks.white.should.have.length(2);
+      state.stacks.white.should.eql([2,1]);
     });
   });
 
+  describe('an invalid card', function() {
+    it('should remove a life', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+      game.playCard(2);
+      const state = game.current();
+      state.lives.should.be.eql(2);
+    });
 
-  describe('playing an invalid card', function() {
+    it('should add the card to the discard pile', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+      const stateBefore = game.current();
 
+      game.playCard(2);
+      const stateAfter = game.current();
+
+      stateAfter.deck.should.have.length(stateBefore.deck.length - 1);
+      stateAfter.discard.should.have.length(stateBefore.discard.length + 1);
+    });
+  });
+});
+
+describe('discarding', function() {
+  it('should add the card to the discard pile', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.start();
+    const stateBefore = game.current();
+
+    game.discardCard(2);
+    const stateAfter = game.current();
+
+    stateAfter.deck.should.have.length(stateBefore.deck.length - 1);
+    stateAfter.discard.should.have.length(stateBefore.discard.length + 1);
+  });
+
+  it('should update the currently playing player', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.start();
+    game.discardCard(0);
+    const state = game.current();
+
+    state.currentPlayer.should.be.eql('nick2');
+  });
+
+  // with the seed 123123123
+  // nick's 3rd card (game.discardCard) is { number: 4, colour: 'green' }
+  describe('a valid card', function() {
+    it('should deal a card back to the players hand', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+      const stateBefore = game.current();
+
+      game.discardCard(2);
+      const stateAfter = game.current();
+
+      stateAfter.deck.should.have.length(stateBefore.deck.length - 1);
+      stateAfter.players.nick.cards.should.have.length(5);
+    });
+
+    it('should add an extra info token', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+
+      // { number: 1, colour: 'green' }
+      game.giveInfo('number', 'nick2', 2);
+
+      var state = game.current();
+      state.info.should.be.eql(7);
+
+      game.discardCard(2);
+
+      state = game.current();
+      state.info.should.be.eql(8);
+    });
+  });
+
+  describe('a card that ends the chance of finishing a stack', function() {
+    // with the seed 123123123
+    it('should set the game to be over', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.start();
+      game.discardCard(0); // nick discards { number: 1, colour: 'white' }
+      game.discardCard(0); // nick2 discards { number: 3, colour: 'green' }
+      game.discardCard(0); // nick discards { number: 1, colour: 'blue' }
+      game.discardCard(0); // nick2 discards { number: 3, colour: 'blue' }
+      game.discardCard(0); // nick discards { number: 4, colour: 'green' }
+      game.discardCard(4); // nick2 discards { number: 5, colour: 'green' }
+
+      const state = game.current();
+      state.gameOver.should.be.eql(true);
+    });
+  });
+});
+
+describe('giving info', function() {
+  it('should update the current player', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.addPlayer('nick3');
+    game.addPlayer('nick4');
+    game.start();
+    game.giveInfo('number', 'nick2', 0);
+
+    const state = game.current();
+
+    state.currentPlayer.should.be.eql('nick2');
+  });
+
+  it('should remove an info token', function() {
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.addPlayer('nick3');
+    game.addPlayer('nick4');
+    game.start();
+
+    game.giveInfo('number', 'nick2', 0);
+    var state = game.current();
+    state.info.should.be.eql(7);
+
+    game.giveInfo('number', 'nick3', 0);
+    state = game.current();
+    state.info.should.be.eql(6);
+  });
+
+  describe('about the number', function() {
+    it('should add the selected card\'s number to the knownNumbers array', function() {
+
+    game.addPlayer('nick');
+    game.addPlayer('nick2');
+    game.addPlayer('nick3');
+    game.addPlayer('nick4');
+    game.start();
+    game.giveInfo('number', 'nick2', 0); // card is { number: 3, colour: 'blue' }
+
+    const state = game.current();
+    state.players.nick2.knownNumbers.should.have.length(1);
+    state.players.nick2.knownNumbers.should.be.eql([3]);
+    });
+  });
+
+  describe('about the colour', function() {
+    it('should add the selected card\'s colour to the knownColours array', function() {
+      game.addPlayer('nick');
+      game.addPlayer('nick2');
+      game.addPlayer('nick3');
+      game.addPlayer('nick4');
+      game.start();
+      game.giveInfo('colour', 'nick2', 0); // card is { number: 3, colour: 'blue' }
+
+      const state = game.current();
+      state.players.nick2.knownColours.should.have.length(1);
+      state.players.nick2.knownColours.should.be.eql(['blue']);
+    });
   });
 });
